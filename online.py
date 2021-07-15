282cc86f9626b9f6e36e3f5ed14707e2adf89ad3
Weronika Plichta <67869741+WeronikaPlichta@users.noreply.github.com>
2021-07-15 23:39:42 +0200

    Update online.py

--- online.py
+++ online.py
@@ -5,9 +5,10 @@ import numpy as np
 import scipy.signal as ss
 from model import *
 
-''''''
-Originally includes a part of code responsible for connecting to amplifier and taking packets of samples from EEG set in real time - not showed here, because this part wasn't written by me.
-''''''
+###
+#Originally includes a part of code responsible for connecting to amplifier and taking packets of samples from EEG set in real time - not showed here, 
+#because this part wasn't written by me.
+###
 
 
 
